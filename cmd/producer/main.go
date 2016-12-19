f34493f2b04081b960932ef8073927f864dfbcd5
Matthias Neugebauer <mtneug@mailbox.org>
2016-12-19 17:01:13 +0100

    Add ability for the producer to report service replica counts

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -15,6 +15,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"math"
@@ -24,19 +26,44 @@ import (
 	"sync"
 	"time"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/net/websocket"
 )
+
+const consumerSrvName = "consumer"
 
 var (
 	store int
 	mutex *sync.Mutex
 	cond  *sync.Cond
 
+	docker          *client.Client
+	desiredReplicas uint64
+	actualReplicas  uint64
+	consumerSrvID   string
+
 	metric prometheus.Gauge
 )
 
 func init() {
+	var err error
+	docker, err = client.NewEnvClient()
+	if err != nil {
+		log.Fatalln("Connection to Docker failed")
+	}
+
+	args := filters.NewArgs()
+	args.Add("name", consumerSrvName)
+	srvs, err := docker.ServiceList(context.TODO(), types.ServiceListOptions{Filter: args})
+	if err != nil || len(srvs) == 0 {
+		log.Fatalln("Connection to Docker failed")
+	}
+	fmt.Printf("%#v\n", srvs)
+	consumerSrvID = srvs[0].ID
+
 	store = 40
 	mutex = &sync.Mutex{}
 	cond = sync.NewCond(mutex)
@@ -46,19 +73,18 @@ func init() {
 		Help: "A demo metric",
 	})
 	metric.Set(float64(store))
+	prometheus.Register(metric)
 }
 
 func main() {
 	go producer()
-
-	prometheus.Register(metric)
 
 	http.Handle("/", http.HandlerFunc(indexHandler))
 	http.Handle("/consume", http.HandlerFunc(consumeHandler))
 	http.Handle("/stats", websocket.Handler(statsHandler))
 	http.Handle("/metrics", prometheus.Handler())
 
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	log.Fatalln(http.ListenAndServe(":5000", nil))
 }
 
 func producer() {
@@ -93,15 +119,52 @@ func consumeHandler(w http.ResponseWriter, r *http.Request) {
 
 func statsHandler(ws *websocket.Conn) {
 	for {
+		err := updateDesiredReplicas()
+		if err != nil {
+			log.Println(err.Error())
+		}
+
+		err = updateActualReplicas()
+		if err != nil {
+			log.Println(err.Error())
+		}
+
 		mutex.Lock()
 		websocket.JSON.Send(ws, map[string]string{
-			"store":    strconv.Itoa(store),
-			"replicas": strconv.Itoa(0),
+			"store":           strconv.Itoa(store),
+			"desiredReplicas": strconv.FormatUint(desiredReplicas, 10),
+			"actualReplicas":  strconv.FormatUint(actualReplicas, 10),
 		})
 		mutex.Unlock()
 
 		<-time.After(2 * time.Second)
 	}
+}
+
+func updateDesiredReplicas() error {
+	srv, _, err := docker.ServiceInspectWithRaw(context.TODO(), consumerSrvID)
+	if err != nil {
+		return errors.New("Counting desiredReplicas failed")
+	}
+
+	srvMode := srv.Spec.Mode
+	if srvMode.Replicated == nil || srvMode.Replicated.Replicas == nil {
+		return errors.New("Not a replicated service")
+	}
+	desiredReplicas = *srv.Spec.Mode.Replicated.Replicas
+	return nil
+}
+
+func updateActualReplicas() error {
+	args := filters.NewArgs()
+	args.Add("label", "com.docker.swarm.service.id="+consumerSrvID)
+	container, err := docker.ContainerList(context.TODO(), types.ContainerListOptions{Filter: args})
+	if err != nil {
+		return errors.New("Counting actualReplicas failed")
+	}
+
+	actualReplicas = uint64(len(container))
+	return nil
 }
 
 const indexPage = `<!DOCTYPE html>
@@ -113,37 +176,51 @@ const indexPage = `<!DOCTYPE html>
   <script type="text/javascript" src="http://smoothiecharts.org/smoothie.js"></script>
   <script type="text/javascript">
     var store = new TimeSeries();
-    var replicas = new TimeSeries();
+    var desiredReplicas = new TimeSeries();
+    var actualReplicas = new TimeSeries();
 
-    var ws = new WebSocket("ws://localhost:5000/stats");
+    var ws = new WebSocket("ws://" + location.host + "/stats");
     ws.onmessage = function(e) {
       var data = JSON.parse(e.data);
       var time = new Date().getTime()
       store.append(time, data.store);
-      replicas.append(time, data.replicas);
+      desiredReplicas.append(time, data.desiredReplicas);
+      actualReplicas.append(time, data.actualReplicas);
     };
 
     function init() {
-      var storeChart = new SmoothieChart();
+      var storeChart = new SmoothieChart({
+        minValue: 0
+      });
       storeChart.addTimeSeries(store, {
         strokeStyle: 'rgba(255, 0, 0, 1)',
         fillStyle: 'rgba(255, 0, 0, 0.2)',
         lineWidth: 4
       });
-      storeChart.streamTo(document.getElementById("storeChart"), 2000);
+      storeChart.streamTo(document.getElementById("storeChart"), 1000);
 
-      var replicaChart = new SmoothieChart();
-      replicaChart.addTimeSeries(replicas, {
+      var replicaChart = new SmoothieChart({
+        minValue: 0,
+        maxValue: 25
+      });
+      replicaChart.addTimeSeries(desiredReplicas, {
+        strokeStyle: 'rgba(0, 0, 255, 1)',
+        fillStyle: 'rgba(0, 0, 255, 0.2)',
+        lineWidth: 4
+      });
+      replicaChart.addTimeSeries(actualReplicas, {
         strokeStyle: 'rgba(0, 255, 0, 1)',
         fillStyle: 'rgba(0, 255, 0, 0.2)',
         lineWidth: 4
       });
-      replicaChart.streamTo(document.getElementById("replicaChart"), 2000);
+      replicaChart.streamTo(document.getElementById("replicaChart"), 1000);
     }
   </script>
 </head>
-<body onload="init()" style="margin: 0">
+<body onload="init()">
+  <h2>Workload</h2>
   <canvas id="storeChart" width="1200" height="400"></canvas>
+  <h2>Number of worker</h2>
   <canvas id="replicaChart" width="1200" height="400"></canvas>
 </body>
 </html>
